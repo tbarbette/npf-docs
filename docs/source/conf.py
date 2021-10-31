45ff9ecf2a03012f0eeddeb830650622c66d1508
Tom Barbette <tom.barbette@uclouvain.be>
2021-10-31 14:46:26 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'NPF'
+copyright = '2021, Tom Barbette'
+author = 'Barbette'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0.18'
 
 # -- General configuration
 
